638f10944c75bc9825ecb406be9a4b76ee626775
Felipe Freire <felipef12123@gmail.com>
2022-11-21 17:19:45 -0300

    Add files via upload

--- src/RmiClient.java
+++ src/RmiClient.java
@@ -1,12 +1,9 @@
-import java.net.MalformedURLException;
 import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 
 public class RmiClient {
 
 	public static void main(String[] args) throws Exception {
-		String objNome = "rmi://localhost:1099/calc";
+		String objNome = "rmi://10.105.2.48:1099/calc";
 		ITFCalculadora calc = (ITFCalculadora) Naming.lookup(objNome);
 		System.out.println("\n---------------------------------------");
 		System.out.println("A soma de 2 + 2 = " + calc.adicao(2, 2));
